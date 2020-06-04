586c2d330757de5f83b444cdd763ce53881e6680
schmock83 <schmock83@protonmail.com>
2020-06-04 09:20:39 +0200

    Ultraschallsensor added

---
+++ UltraschallSensor/ultraschallsensor_entfernung.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <wiringPi.h>
+
+#define GPIO_TRIGGER 1
+#define GPIO_ECHO 5
+
+double distance_cm(void)
+{
+    digitalWrite(GPIO_TRIGGER, HIGH);
+
+    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
+
+    digitalWrite(GPIO_TRIGGER, LOW);
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+    auto stopTime = std::chrono::high_resolution_clock::now();
+
+    while(digitalRead(GPIO_ECHO) == LOW)
+    {
+        startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    while (digitalRead(GPIO_ECHO) == HIGH)
+    {
+        stopTime = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed_ms = std::chrono::duration<double, std::milli>(stopTime - startTime).count();
+
+    return (elapsed_ms * 34.3) / 2;
+}
+
+int main()
+{
+
+    //Richtung der GPIO - Pins festlegen(IN / OUT)
+    wiringPiSetup();
+
+    pinMode(GPIO_TRIGGER, OUTPUT);
+    pinMode(GPIO_ECHO, INPUT);
+
+    while(true)
+    {
+        std::cout << "Distance: " << std::fixed << std::setprecision(2) << distance_cm() << "cm\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    return 0;
+}
