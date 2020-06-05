499f13b51aa3d306d2d194d4942648bdc65837e9
schmock83 <schmock83@protonmail.com>
2020-06-05 16:55:15 +0200

    speed angepasst vom linken motor

---
+++ UltraschallSensor/source/adafruitController.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "adafruitmotorhat.h"
+
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+/*
+* Wrapper Klasse die den MotorHat steuert und indirekt die motoren verwaltet
+*/
+
+class AdafruitController
+{
+public:
+    enum ControllerCommand
+    {
+        kForward,
+        kBackward,
+        kBrake,
+        kRelease,
+        kLeft,
+        kRight,
+        kInvalidCommand = -1,
+    };
+    ~AdafruitController();
+    void setSpeed(int speed=100);
+    void setSpeed(int speed, int index);
+    bool motorsAvailable(void);
+    bool isValid(void);
+
+    void drive(ControllerCommand command);
+
+private:
+    AdafruitMotorHAT hat;
+};
