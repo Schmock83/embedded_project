cb34f078257c76bbb3843f6a9818099f0a0d04e4
Benedikt Happe <benedikt.happe@alumni.fh-aachen.de>
2021-04-29 16:22:49 +0200

    Prak2 hinzugefügt

---
+++ Prak2_Treiber/Src/GPIO_Driver.c
@@ -0,0 +1,159 @@
+/*
+ * GPIO_Driver.c
+ * Vorlage fürs Praktikum 
+ */
+
+#include "GPIO_Driver.h"
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_EN();
+		}else if (pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_EN();
+		}else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_EN();
+		}else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_EN();
+		}else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_EN();
+		}else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_EN();
+		}else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_EN();
+		}else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_EN();
+		}else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_EN();
+		}
+	}
+	else
+	{
+		// Implementieren Sie die Funktionalität der Clock Deaktivierung
+	}
+}
+
+// Implementieren Sie die weiteren Schritte. Denken Sie daran die Einträge eines Registers zurückzusetzen, bevor Sie einzelne Bits setzen!
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	 //Anschalten der Clock
+	 GPIO_PeriClockControl(pGPIOHandle->pGPIOx, ENABLE);
+
+	 //Konfigurieren des PIN in INPUT oder OUTPUT-Modus
+	 if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	 {
+		// Implementieren Sie die Funktionalität
+	 }
+	 
+	 // ####################################### Praktikum 3: IRQ ############################################################
+	 
+	 else
+	 { 
+	    // IRQ-Modus (Muss erst im dritten Praktikum bearbeitet werden)
+		// 1. Konfiguration für Trigger auf fallenende, steigende oder beide Flanken
+		//2. Konfiguration des entsprechenden GPIO-Ports in SYSCFG_EXTICR
+		//3  Aktivieren des EXTI Interrupts handling in IMR-Register
+	 }
+	 
+	// ####################################### ENDE IRQ #####################################################################
+	
+	 //Konfigurieren des Output-Speeds
+	 
+	 //Konfigurieren des Pull-up/down Settings
+	 
+	 //Konfigurieren des Output modus
+	 
+}
+
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+	if(pGPIOx == GPIOA)
+	{
+		GPIOA_REG_RESET();
+	}else if (pGPIOx == GPIOB)
+	{
+		GPIOB_REG_RESET();
+	}else if (pGPIOx == GPIOC)
+	{
+		GPIOC_REG_RESET();
+	}else if (pGPIOx == GPIOD)
+	{
+		GPIOD_REG_RESET();
+	}else if (pGPIOx == GPIOE)
+	{
+		GPIOE_REG_RESET();
+	}else if (pGPIOx == GPIOF)
+	{
+		GPIOF_REG_RESET();
+	}else if (pGPIOx == GPIOG)
+	{
+		GPIOG_REG_RESET();
+	}else if (pGPIOx == GPIOH)
+	{
+		GPIOH_REG_RESET();
+	}else if (pGPIOx == GPIOI)
+	{
+		GPIOI_REG_RESET();
+	}
+}
+
+uint8_t GPIO_ReadFromInputPin(GPIO_Handle_t *pGPIO_Handle)
+{
+	// Implementieren Sie die Funktionalität
+}
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+	// Implementieren Sie die Funktionalität
+}
+
+void GPIO_WriteToOutputPin(GPIO_Handle_t *pGPIO_Handle, uint8_t Value)
+{
+	// Implementieren Sie die Funktionalität
+}
+
+
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
+{
+	// Implementieren Sie die Funktionalität
+}
+
+
+void GPIO_ToggleOutputPin(GPIO_Handle_t *pGPIO_Handle)
+{
+	// Implementieren Sie die Funktionalität
+}
+
+// ####################################### Praktikum 3: IRQ ############################################################
+
+void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+	// Hinweis: Bedenken Sie welche ISER und ICER Register des Prozessors in dem verwendeten Mikrocontroller benutzt werden können.
+	// Nicht alle Interrupts sind im Mikrocontroller aktiv. Überprüfen sie dazu das Handbuch (Reference Manual) des Mikrocontrollers.
+	if(EnorDi == ENABLE)
+	{
+		//ToDo: Programmieren der entsprechenden ISERx register
+	}
+	else
+	{
+		//ToDo: Programmieren der entsprechenden ICERx register
+	}
+}
+
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+	// Abfrage und zurücksetzen des EXTI-PR bits
+}
+
+// ####################################### ENDE IRQ ###################################################################
